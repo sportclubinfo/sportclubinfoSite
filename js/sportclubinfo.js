0a405c5839d5cf7d7558e3f43edda22f68c1b924
Jeroen <Jeroen@Jeroen>
2014-12-01 23:15:24 +0100

    Merge origin/master
    Conflicts:
    	css/signin.css
    	includes/checkLogin.php
    	includes/header.php
    	includes/login.php
    	index.php
    	php/addSpeler.php
    	php/addVereniging.php
    	php/home.php

--- js/sportclubinfo.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function($){
-$('#sidemenu').scrollNav({
-    showHeadline: false,
-    showTopLink: false,
-    scrollOffset: 0,
-    fixedMargin: 60,
-    sectionElem: 'h2'
-});
-});
